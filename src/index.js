0fc30a3915273738226def5af902ec8cdcad6326
olena-semenenko <elen.kinakh@gmail.com>
2022-12-04 13:12:05 +0100

    Added progect

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './js/header-scroll';
+import './js/anchor-scroll';
+import './js/simplelightbox';
+import './js/slick';
+import './js/mobile-menu';
+import "./js/modal";
+import "./js/modal-1";
+import "./js/modal-2";
+import "./js/modal-3";
+import "./js/modal-4";
+import "./js/modal-5";
+import "./js/slide";
+import "./js/slider";
+import "./js/modalbuynow2";
+import "./js/modalreadmore";
+
+
+
+
